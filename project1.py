857b14d34d462e27c8e7d2fda4b9ee78589d72e4
Quentin M <mattson.quentin@gmail.com>
2022-09-21 21:07:00 -0700

    guess between 1 - 10

--- project1.py
+++ project1.py
@@ -11,11 +11,12 @@ def main():
 
     # Print out some welcome text, and some instructions on how to play the game
     print("Welcome to Guess the Number Game!")
-    print("Guess a number from 1 to 100.")
+    print("Guess a number from 1 to 10.")
     print("Input must be a numeric digit. Characters are not allowed.\n")
 
     # Generate a random number, convert it to a string, save it in the variable 'target'
-    target = str(random.randrange(1, 101))
+    target = str(random.randrange(1, 11))
+    print(target)
 
     # Keep track of number of guesses
     number_of_guesses = 0
@@ -23,7 +24,7 @@ def main():
     # Loop until user has finished guessing 5 times
     while number_of_guesses != 5:
         # Ask for user input
-        guess = input("Guess a number from 1 - 100: ")
+        guess = input("Guess a number from 1 - 10: ")
 
         # Increase number of guesses by 1
         number_of_guesses += 1
@@ -35,7 +36,7 @@ def main():
         
         # Check if guess is not a digit
         elif not guess.isdigit():
-            print("Please enter a number")
+            print("Please enter a number\n")
 
         # Check if guess is correct
         elif guess == target:
@@ -44,7 +45,7 @@ def main():
         
         # If guess is incorrect, then prompt to guess again
         else:
-            print("Incorrect, please guess again")
+            print("Incorrect, please guess again\n")
 
 # If conditional that calls the main function
 if __name__ == "__main__":
